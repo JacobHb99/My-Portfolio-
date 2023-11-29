9ea532012bf813f57d14c706cce1159417307130
JacobHb99 <jacob.hb@live.de>
2023-11-29 17:38:21 +0100

    contact.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 
@@ -9,20 +10,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
   public contactForm = new FormGroup(
     {
       nameField: new FormControl('', [Validators.required, Validators.minLength(2)]),
       emailField: new FormControl('', [Validators.required, Validators.email]),
       messageField: new FormControl('', [Validators.required, Validators.minLength(10)]),
-      checkBox: new FormControl(false)
+      checkBox: new FormControl(false, [Validators.requiredTrue])
     }
   );
+    
 
   constructor(public home: HomeComponent) {
+    //console.log('form', this.contactForm.get('nameField').value);
   }
 
+
+  ngOnInit(): void {
+    
+  }
 
   sendMail() {
 /*     console.log(this.myForm);
@@ -31,31 +38,23 @@ export class ContactComponent {
     let messageField = this.messageInput.nativeElement;
     let senMailBtn = this.sendMailBtn.nativeElement;
 
-    nameField.disabled = true;
-    emailField.disabled = true;
-    messageField.disabled = true;
-    senMailBtn.disabled = true;
-
 
     let formData = new FormData();
-    formData.append('name', nameField.value);
-    formData.append('email', emailField.value);
-    formData.append('message', messageField.value);
-    console.log(formData); */
-
-
-    formData = [{
-      name: this.contactForm.
-    }]
+    formData.append('name', this.contactForm.get('nameField').value);
+    formData.append('message', this.contactForm.get('messageInput').value);
+    formData.append('email', this.contactForm.get('emailInput').value);
+ 
+    console.log(formData); 
+  
     //senden
      fetch("https://jacob-hengsbach.com/send_mail/send_mail.php",
       {
         method: 'POST',
-        body: 
+        body: formData
       }
     ) 
 
-
+*/ 
     
   }
 
